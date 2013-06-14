89c02eb634619915625d39847cd0877d18eb4494
cube3_cp <SND\cube3_cp>
2013-06-14 16:13:51 +0000

    Adding Templated Configuration to Instances.

---
+++ VpNet/trunk/VpNet/ManagedApi/System/InstanceConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using VpNet.Interfaces;
+
+namespace VpNet
+{
+    [Serializable]
+    public class InstanceConfiguration<TWorld> : Abstract.BaseInstanceConfiguration<TWorld>
+        where TWorld : class, IWorld, new()
+    {
+    }
+}
