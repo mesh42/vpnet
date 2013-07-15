9d7fd8720d83fd1ac86209306973aaeb8482fc66
cube3_cp <SND\cube3_cp>
2013-07-15 14:07:39 +0000

    Adding T4 templates for calculating revision in number of day's since december 20th 2010.

--- VpNet/trunk/VpNet/Properties/AssemblyInfo.cs
+++ VpNet/trunk/VpNet/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("VPNet")]
-[assembly: AssemblyCopyright("Copyright © Cube3 2012-2013")]
+[assembly: AssemblyCopyright("Copyright © Cube3 2012-2013 LGPL")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,16 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b9693da9-add2-45e4-841e-9b72e55812a5")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
