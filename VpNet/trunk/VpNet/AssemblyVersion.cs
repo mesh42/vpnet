d57fef28e04b4f32c0db27922c3bf2d328ab124a
cube3_cp <SND\cube3_cp>
2014-04-24 18:39:57 +0000

    Relase associated with Nuget 1.0.3.1222

--- VpNet/trunk/VpNet/AssemblyVersion.cs
+++ VpNet/trunk/VpNet/AssemblyVersion.cs
@@ -5,5 +5,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.3.1216")]
-[assembly: AssemblyFileVersion("1.0.3.1216")]
+[assembly: AssemblyVersion("1.0.3.1222")]
+[assembly: AssemblyFileVersion("1.0.3.1222")]
