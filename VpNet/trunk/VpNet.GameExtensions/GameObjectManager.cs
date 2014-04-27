452e7fd6eb13a86822325899e49cf3a692c706f4
cube3_cp <SND\cube3_cp>
2014-04-27 21:06:56 +0000

    Adding CS scriptable objects and Game Scene Node hiararchy (WIP)

--- VpNet/trunk/VpNet.GameExtensions/GameObjectManager.cs
+++ VpNet/trunk/VpNet.GameExtensions/GameObjectManager.cs
@@ -1,11 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Reactive;
 using System.Reactive.Linq;
+using CSScriptLibrary;
 using VpNet.Extensions;
+using VpNet.GameExtensions.Interfaces;
 
 namespace VpNet.GameExtensions
 {
+    public class GameScript
+    {
+        private readonly GameInstance _instance;
+        public HostApp Host;
+        public FileSystemWatcher _fsw;
+        private GameVpObject _vpObject;
+        private readonly string _csScriptName;
+        private IGameScriptVpObject scriptObject = null;
+
+        public GameScript(GameInstance instance, GameVpObject vpObject, string csScriptName)
+        {
+            _instance = instance;
+            _vpObject = vpObject;
+            _csScriptName = csScriptName;
+            _fsw = new FileSystemWatcher(Path.Combine(instance.GameInstanceConfiguration.StorageDataPath, "CsScript"), _csScriptName);
+            _fsw.Changed += _fsw_Changed;
+            _fsw.EnableRaisingEvents = true;
+            _fsw.NotifyFilter = NotifyFilters.LastWrite;
+            Execute(_csScriptName);
+        }
+
+        private void Execute(string csScriptName)
+        {
+            var script1 = Path.Combine(_instance.GameInstanceConfiguration.StorageDataPath, "CsScript", csScriptName).LoadTextFile();
+                Host= new HostApp();
+                scriptObject = CSScript.LoadCode(script1)
+                                .CreateObject("*")
+                                .AlignToInterface<IGameScriptVpObject>();
+
+            scriptObject.GameVpObject = _vpObject;
+            scriptObject.GameInstance = _instance;
+            scriptObject.Initialize();
+        }
+
+
+        void _fsw_Changed(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType == WatcherChangeTypes.Changed)
+            {
+                _fsw.EnableRaisingEvents = false;
+                try
+                {
+                    _instance.Say("changed");
+                }
+
+                finally
+                {
+                    _fsw.EnableRaisingEvents = true;
+                }
+                scriptObject.Unload();
+                
+                scriptObject = null;
+                Host = null;
+                Execute(_csScriptName);
+
+            }
+        }
+    }
+
     /// <summary>
     /// Manages objects and tracks changes by applying some filtering on top of the underlaying VPNET callbacks.
     /// </summary>
@@ -14,6 +76,8 @@ namespace VpNet.GameExtensions
     {
         private readonly GameInstance _instance;
         private Dictionary<int,GameVpObject> _list = new Dictionary<int, GameVpObject>();
+        private Dictionary<int, GameScript> _csScripts = new Dictionary<int, GameScript>();
+         
 
         public delegate void OnManagedObjectChangedCallbackDelegate(GameInstance instance, ObjectChangeCallbackArgsT<RcDefault, GameVpObject, Vector3> args);
         public delegate void OnManagedObjectChangedDelegate(GameInstance instance, ObjectChangeArgsT<GameAvatar, GameVpObject, Vector3> args);
@@ -68,6 +132,9 @@ namespace VpNet.GameExtensions
 
         public GameObjectManager(GameInstance instance)
         {
+           
+            
+
             _instance = instance;
             instance.OnObjectChange += instance_OnObjectChange;
             instance.OnObjectChangeCallback += instance_OnObjectChangeCallback;
@@ -97,6 +164,36 @@ namespace VpNet.GameExtensions
         {
             foreach (var prototype in prototypes)
                 Add(prototype);
+        }
+
+        public void AttachCsScript(GameVpObject gameVpObject, string csScriptName)
+        {
+            if (!_list.ContainsKey(gameVpObject.Id))
+            {
+                var script = Path.Combine(_instance.GameInstanceConfiguration.StorageDataPath, "CsScript", csScriptName).LoadTextFile();
+                Add(gameVpObject, script);
+            }
+            _csScripts.Add(gameVpObject.Id, new GameScript(_instance, gameVpObject, csScriptName));
+        }
+
+
+        public void RemoveCsScript(GameVpObject gameVpObject)
+        {
+            if (_csScripts.ContainsKey(gameVpObject.Id))
+            {
+                var host = _csScripts[gameVpObject.Id];
+                host = null;
+                _csScripts.Remove(gameVpObject.Id);
+            }
+        }
+
+        public RcDefault Add(GameVpObject prototype, string attachCsScript)
+        {
+            if (_list.ContainsKey(prototype.Id))
+                return new RcDefault(0);
+            _list.Add(prototype.Id, prototype);
+            // refresh the object.
+            return _instance.GetObject(prototype.Id);
         }
 
         public RcDefault Add(GameVpObject prototype)
---
+++ VpNet/trunk/VpNet.GameExtensions/Interfaces/IGameScriptVpObject.cs
@@ -0,0 +1,10 @@
+﻿namespace VpNet.GameExtensions.Interfaces
+{
+    public interface IGameScriptVpObject
+    {
+        GameInstance GameInstance { get; set; }
+        GameVpObject GameVpObject { get; set; }
+        void Initialize();
+        void Unload();
+    }
+}
