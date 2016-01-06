c459117b5244af45e42dcde5e357eef05a162a8d
cube3_cp <SND\cube3_cp>
2016-01-06 23:21:43 +0000

    Release associated with nuget package 1.0.8.1844

--- VpNet/trunk/VpNet/AssemblyVersion.cs
+++ VpNet/trunk/VpNet/AssemblyVersion.cs
@@ -5,5 +5,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.8.1843")]
-[assembly: AssemblyFileVersion("1.0.8.1843")]
+[assembly: AssemblyVersion("1.0.8.1844")]
+[assembly: AssemblyFileVersion("1.0.8.1844")]
