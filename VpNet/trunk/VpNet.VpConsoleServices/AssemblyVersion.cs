aa11cc65c92973e10afa0b035b3230919b776d80
cube3_cp <SND\cube3_cp>
2016-05-14 20:30:58 +0000

    Associated with release 1.0.9.1973-alpha

--- VpNet/trunk/VpNet.VpConsoleServices/AssemblyVersion.cs
+++ VpNet/trunk/VpNet.VpConsoleServices/AssemblyVersion.cs
@@ -5,5 +5,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.9.1968")]
-[assembly: AssemblyFileVersion("1.0.9.1968")]
+[assembly: AssemblyVersion("1.0.9.1973")]
+[assembly: AssemblyFileVersion("1.0.9.1973")]
